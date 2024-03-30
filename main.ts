1c70557ae62f8f57be92d229a7a5cfa4dc067ca4
kumail132 <kumailz132@gmail.com>
2024-03-31 04:18:00 +0500

    number guesing game

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("Wellcome To - Cli Number Guessing Game");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    { 
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter A Guess Number Between 1-5 ?",
+
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Yes ! You Guess A Correct Number" ,`${randomNumber}`);
+    
+}
+else {
+    console.log("You Guess A Wrong Number");
+    
+}
